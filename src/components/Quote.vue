a0cdc78009fec38753990f930e6270b234eb69a4
Nismoteika <stepanpahska46@gmail.com>
2021-02-18 19:28:35 +0300

    edit layout component quote

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,9 +1,11 @@
 <template>
-  <v-card class="quote mb-2">
-    <v-card-title>{{ author }}</v-card-title>
-    <v-card-text>{{ text }}</v-card-text>
-    <v-card-text>{{ date }}</v-card-text>
-  </v-card>
+  <v-col cols="12">
+    <v-card class="quote mb-2">
+      <v-card-title>{{ author }}</v-card-title>
+      <v-card-text>{{ text }}</v-card-text>
+      <v-card-text>{{ date }}</v-card-text>
+    </v-card>
+  </v-col>
 </template>
 
 <script>
